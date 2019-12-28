0bfca41fcd789579d63aed65dc19305e21fe1743
kwintenvdb <kwintenvdberghe@gmail.com>
2019-12-28 23:54:24 +0100

    Initial commit

---
+++ lib/getFontInfo.ts
@@ -0,0 +1,4 @@
+export function getFontInfo(element: Element) {
+    const style = getComputedStyle(element);
+    console.log(style);
+}
---
+++ lib/index.ts
@@ -0,0 +1,10 @@
+console.log('test');
+
+import { getFontInfo } from './getFontInfo';
+
+// getFontInfo();
+
+document.addEventListener('mouseover', e => {
+    console.log(e.target);
+});
+
